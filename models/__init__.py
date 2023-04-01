f59d7819e104c7ce88cbf0f8f8c4b9021a0bd4f7
Byron Ochieng <byronochieng01@gmail.com>
2023-04-01 20:27:14 +0300

    created instance of FileStorage and called reload() method

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,8 @@
 __init__.py is a special python file that is
 used to mark a directory as a python package
 """
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
