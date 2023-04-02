9e8c6bbfa12cef593c682b53ff5c621a2a2d9574
Byron Ochieng <byronochieng01@gmail.com>
2023-04-02 13:23:36 +0300

    linked BaseModel to FileStorage using the variable storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3.10
 """
 Base Model module:
 defines all common attributes/methods for other classes
@@ -6,7 +6,7 @@ defines all common attributes/methods for other classes
 
 import datetime
 import models
-import  uuid
+import uuid
 
 
 class BaseModel:
@@ -42,6 +42,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -59,6 +60,7 @@ class BaseModel:
         updates 'updated_at' attribute with current datetime
         """
         self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
